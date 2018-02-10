df1dc6a8939ca5b384bcba098e6847150f5283fe
huangjoyce3 <jhuang3@uw.edu>
2018-02-09 16:33:42 -0800

    rm default app route

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
 import SignIn from '@/components/SignIn'
 
 Vue.use(Router)
@@ -9,11 +8,6 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    },
-    {
-      path: '/login',
       name: 'SignIn',
       component: SignIn
     }
