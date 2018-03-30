1b44df59e36957d858bd94e192657a3a8adb89d1
huangjoyce3 <jhuang3@uw.edu>
2018-03-30 13:38:23 -0700

    merge wufoo

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,20 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import SignIn from '@/components/SignIn'
-
+import FormList from '@/components/FormList'
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
       path: '/',
-      name: 'SignIn',
+      name: 'auth',
       component: SignIn
+    },
+    {
+      path: '/home',
+      name: 'home',
+      component: FormList
     }
   ]
 })
