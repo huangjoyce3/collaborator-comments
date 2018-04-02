9d3bd95546ce6dff976c94154f73d83bf850c8bf
huangjoyce3 <jhuang3@uw.edu>
2018-04-01 19:28:28 -0700

    style

--- src/components/FormList.vue
+++ src/components/FormList.vue
@@ -3,9 +3,6 @@
         <div class="header">
             <p>Form Name</p>
             <p v-bind:click="reverse">Date Created</p>
-            <p>Total Entries</p>
-            <p>Entries Today</p>
-            <p>Date Edited</p>
         </div>
         <item v-for="form in forms" v-bind:key="form.id" :form.sync="form"></item>
     </div>
@@ -17,11 +14,11 @@ import Item from './Item';
 export default {
     name: 'form-list',
     components: {Item,},
-    // methods: { 
-    //     reverse () {
-    //         this.forms.reverse();
-    //     }
-    // },
+    methods: { 
+        reverse () {
+            this.forms.reverse();
+        }
+    },
     data(){
         return {
             forms: [{
@@ -29,19 +26,16 @@ export default {
                 dateCreated: "12/13/18",
                 entries: 200,
                 entriesToday: 1,
-                dateEdited: "12/15/18",
             }, {
                 title: "GBD Paper 2",
                 dateCreated: "10/10/18",
                 entries: 120,
                 entriesToday: 13,
-                dateEdited: "10/30/18",
             }, {
-                title: "GBD Paper 1",
+                title: "Really long long long name here 2018",
                 dateCreated: "4/13/18",
                 entries: 18,
                 entriesToday: 0,
-                dateEdited: "4/13/18",
             }],
         };
     },
@@ -53,6 +47,8 @@ export default {
     margin-left: 50px;
     margin-right: 50px;
     border-bottom: 1px solid #DDDDDD;
+    width: 80%;
+    margin: auto;
 }
 
 .filter{
@@ -61,6 +57,9 @@ export default {
 
 .header{
     display: flex;
+    font-weight: 500;
+    justify-content: flex-start;
+    margin-left: 20px;
 }
 
 .header p{
