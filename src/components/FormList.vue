75336302c76547cb3759f6f4eefe291cd87e8412
huangjoyce3 <jhuang3@uw.edu>
2018-04-20 10:04:28 -0700

    dummy test filter

--- src/components/FormList.vue
+++ src/components/FormList.vue
@@ -1,42 +1,54 @@
 <template>
-    <div class="form-list">
-        <!-- <div class="header">
-            <p>Form Name</p>
-            <p v-bind:click="reverse">Date Created</p>
-        </div>
-        <item v-for="form in forms" v-bind:key="form.id" :form.sync="form"></item> -->
-        <form id="search">
-            Search: <input name="query" v-model="searchQuery">
-        </form>
+    <!-- <div class="form-list">
         <table>
             <thead>
             <tr>
-                <th v-for="header in formHeaders" v-bind:key="header.id">
-                {{ header  }}
-                <span class="arrow" >
-                </span>
-                </th>
-                <!-- <th class="column1">Form Name</th>
-                <th>Date Created</th> -->
+                <th class="column1">Form Name</th>
+                <th>Date Created</th>
             </tr>
             </thead>
             <tbody>
-            <tr v-for="form in testForms" v-bind:key="form.id">
+            <tr v-for="form in forms" v-bind:key="form.id">
                 <td class="column1">
                     {{form.Name}}
                 </td>
                 <td>
                     {{form.DateCreated.split(" ")[0]}}
                 </td>
-                <!-- <td>
+                <td>
                 {{getEntries(form.Url)}}
-                </td> -->
+                </td>
                 <td>
                     <div v-on:click="onClick(form.Url)" id="export" class="button">Export</div>
                 </td>
             </tr>
             </tbody>
-        </table>
+        </table>:data="[
+     { name: 'Authorship form: GBD 2016 Firearms Paper', dateCreate: '2016-09-23', totalEntries: 337 },
+     { name: 'Authorship form: GBD 2016 HAQ paper', dateCreate: '2018-02-28', totalEntries: 68 },
+     { name: 'Collaborator Comments: GBD 2015 Updated', dateCreate: '2018-03-23', totalEntries: 922 },
+     { name: 'Comment Form: Alcohol use and burden paper', dateCreate: '2016-10-28', totalEntries: 270 },
+     { name: 'Injuries, and Risk Factors Study 2016', dateCreate: '2017-7-21', totalEntries: 270 },
+     { name: 'Funding Universal Health Coverage and the Unfinished HIV/AIDS Agenda', dateCreate: '2019-1-13', totalEntries: 47 },
+     { name: 'Spending on health and HIV/AIDS: domestic health spending and development assistance', dateCreate: '2018-03-28', totalEntries: 543 },
+     { name: 'Trends in future health financing and coverage', dateCreate: '2014-02-03', totalEntries: 232 },
+     ]"
+    </div> -->
+    <div class="form-list">
+        <table-component
+     :data="fetchData"
+     sort-by="totalEntries"
+     sort-order="asc"
+     >
+     <table-column show="name" label="Name"></table-column>
+     <table-column show="totalEntries" label="Total Entries" data-type="numeric"></table-column>
+     <table-column show="dateCreate" label="Date Created" :filterable="false" data-type="date:YYYY/MM/DD"></table-column>
+     <table-column label="" :sortable="false" :filterable="false">
+         <template slot-scope="row">
+            <div v-on:click="onClick('comment-form-gbd-2016-cancer-paper')" id="export" class="button">Export</div>
+         </template>
+     </table-column>
+ </table-component>
     </div>
 </template>
 
@@ -63,8 +75,17 @@ export default {
                 // return this.entries;
             })
         },
+        async fetchData({ page, filter, sort }) {
+            let url = "http://localhost:3000/allForms";
+            var response = await axios.get(url, { page });
+            // response.data=response.data.replace(/"(\w+)"\s*:/g, '$1:');
+            console.log(response.data)
+            // An object that has a `data` and an optional `pagination` property
+            return response;
+        },
+
         onClick(formURL){
-            // alert(this.form.Name);
+            alert(formURL);
             let sheetID = '15vAkt-aI_m18rZOzz7qRfIl1fAXhsX0BGVcYUGBIjAo'
             let url = "http://localhost:3000/form/" + formURL + '/' + sheetID;
             //let url = 'http://localhost:3000/form/comment-form-gbd-2016-cancer-paper/1mKw1_QfofAOhJt-gud-5Trphct9hYtZHleit7l1eITU';
@@ -76,18 +97,20 @@ export default {
                 alert('Failed to export to Google Sheets');
                 console.log(error);
             })
+
+            // TODO: Save entry count locally
         }
     },
     data(){
         return {
             forms: [],
-            formHeaders: ['Name', 'Date'],
-            testForms: [
-                { Name: 'Collaborator Comments: GBD 2015 Updated', DateCreated: '1/2/2018' , url: 'form-name', totalEntries: 122},
-                { Name: 'Comment Form: Cause of death preliminary estimates', DateCreated: '1/22/2017' , url: 'form-name', totalEntries: 78},
-                { Name: 'Comment Form: EMR Obesity Paper', DateCreated: '10/8/2016' , url: 'form-name', totalEntries: 0},
-                { Name: 'Comment Form: EMR HIV', DateCreated: '12/14/2018' , url: 'form-name', totalEntries: 53},
-            ],
+            // formHeaders: ['Name', 'Date'],
+            // testForms: [
+            //     { Name: 'Collaborator Comments: GBD 2015 Updated', DateCreated: '1/2/2018' , url: 'form-name', totalEntries: 122},
+            //     { Name: 'Comment Form: Cause of death preliminary estimates', DateCreated: '1/22/2017' , url: 'form-name', totalEntries: 78},
+            //     { Name: 'Comment Form: EMR Obesity Paper', DateCreated: '10/8/2016' , url: 'form-name', totalEntries: 0},
+            //     { Name: 'Comment Form: EMR HIV', DateCreated: '12/14/2018' , url: 'form-name', totalEntries: 53},
+            // ],
             searchQuery: '',
         };
     },
@@ -129,9 +152,10 @@ table{
 th, td{
      border-bottom: 1px solid #DDDDDD;
      padding: 10px;
+     text-align: left;
 }
-.form-list{
-    border-bottom: 1px solid #DDDDDD;
+tbody{
+    text-align: left;
 }
 .column1{
     text-align: left;
@@ -148,7 +172,7 @@ th, td{
 }
 
 .header p{
-    margin-right: 20px;
+    margin-right: 20spx;
 }
 
 .buttons{
