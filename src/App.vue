fa770a2ab1269180af62304de63a9d8660f3a40f
huangjoyce3 <jhuang3@uw.edu>
2018-05-03 03:28:14 -0700

    tabs style

--- src/App.vue
+++ src/App.vue
@@ -5,22 +5,34 @@
       <img src="./assets/ihme-logo.png">
     </router-link>
     <p id="name">IHME Collaborator</p>
-    <router-link :to="{name: 'home'}">
-        <button class="button">Form Manager</button>
-    </router-link>
-    <router-link :to="{name: 'assign'}">
-        <button class="button">Assign</button>
-    </router-link>
-    <router-link :to="{name: 'wordbank'}">
-        <button class="button">Wordbank</button>
-    </router-link>
+    <div class="tab-wrapper">
+      <router-link :to="{name: 'home'}">
+          <button class="tab" :class="{highlight: highlighted}" @click="toggleHighlight">Form Manager</button>
+      </router-link>
+      <router-link :to="{name: 'assign'}">
+          <button class="tab" :class="{highlight: highlighted}" @click="toggleHighlight">Assign</button>
+      </router-link>
+      <router-link :to="{name: 'wordbank'}">
+          <button class="tab" :class="{highlight: highlighted}" @click="toggleHighlight">Word Bank</button>
+      </router-link>
+    </div>
     <router-view/>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
+  name: 'App',
+  data(){
+    return{
+      highlighted: true
+    };
+  },
+  methods:{
+    toggleHighlight(){
+      this.hightlighted = !this.highlighted
+    }
+  }
 }
 </script>
 
@@ -34,6 +46,20 @@ export default {
   margin-top: 0px;
 }
 
+.tab{
+  border: none;
+  outline: none;
+  font-size: 1.6vw;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  margin-right: 20px;
+  cursor: pointer;
+}
+.tab-wrapper{
+  margin-bottom: 30px;
+}
+.highlight{
+  border-bottom:2px solid #8AB76B;
+}
 .header-wrapper{
   display: flex;
 }
